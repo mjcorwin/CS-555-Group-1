0348cd48cbb4fa913b982bb557c9543ec6c2f11d
Terrance Peters <terrancejpeters@gmail.com>
2022-10-10 22:14:00 -0400

    US 08 and 07 (#7)
    * User Story 07 implemented
    
    * US07 implemented in Main
    
    * US 08 Implemented

--- gedcom/Tests/__init__.py
+++ gedcom/Tests/__init__.py
@@ -2,3 +2,6 @@ import gedcom.Tests.US03;
 import gedcom.Tests.US04;
 import gedcom.Tests.US05;
 import gedcom.Tests.US06;
+import gedcom.Tests.US07;
+import gedcom.Tests.US08;
+
--- main.py
+++ main.py
@@ -26,6 +26,10 @@ def main():
             parser.families.values(), parser.individuals
         )
 
+        LT_150 = Tests.gedcom.Tests.US07.Execute(parser);
+
+        BIR_BF_MARR_AF_DIV = Tests.gedcom.Tests.US08.Execute(parser);
+
         print("INDIVIDUALS")
         print(individuals_table)
         print("FAMILIES")
@@ -37,10 +41,17 @@ def main():
         print("DIVORCE BEFORE DEATH")
         print(divorce_before_death)
 
+        print("LESS THAN 150 YEARS OLD")
+        print(LT_150)
+
+        print("Born before marriage or after divorce")
+        print(BIR_BF_MARR_AF_DIV)
+
         outfile.write("INDIVIDUALS\n")
         outfile.write(individuals_table)
         outfile.write("\nFAMILIES\n")
         outfile.write(family_table)
+
         outfile.write("\nVALIDATIONS\n")
         outfile.write(validations)
         outfile.write("\n")
@@ -48,8 +59,18 @@ def main():
         outfile.write(marriage_before_death)
         outfile.write("\nDIVORCE BEFORE DEATH\n")
         outfile.write(divorce_before_death)
+        
+        #outfile.write("\nVALIDATIONS\n")
+        #outfile.write(validations)
+        #outfile.write("\n")
+        outfile.write("\nLESS THAN 150 YEARS OLD\n")
+        outfile.write(LT_150)
+
+        outfile.write("\nBorn before marriage or after divorce\n")
+        outfile.write(BIR_BF_MARR_AF_DIV)
 
     Run_Tests(parser)
+
 
 
 def Run_Tests(hParser):
@@ -57,6 +78,9 @@ def Run_Tests(hParser):
     Tests.US04.Execute(hParser);
     Tests.US05.Execute(hParser);
     Tests.US06.Execute(hParser);
+    Tests.US07.Execute(hParser);
+    Tests.US08.Execute(hParser);
+
 
 if __name__ == "__main__":
     main()
