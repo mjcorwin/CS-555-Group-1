369baf38b35ecbb991873681a5322a00e90b02e6
Jesus <45210927+toxtlej1@users.noreply.github.com>
2022-10-10 20:49:53 -0400

    (feat): US05-US06 - marriage/divorce before death (#6)
    * (feat): US06 - divorce before death
    
    * Forgot to include this additional change per US06
    
    * (feat): US05 - Marriage before death
    
    * (fix): US05 - resolved minor naming convention
    
    * (fix): resolved data failure type
    
    * (fix): US05-06: return deliverable in .txt

--- gedcom/Tests/__init__.py
+++ gedcom/Tests/__init__.py
@@ -1,2 +1,4 @@
 import gedcom.Tests.US03;
 import gedcom.Tests.US04;
+import gedcom.Tests.US05;
+import gedcom.Tests.US06;
--- main.py
+++ main.py
@@ -17,6 +17,10 @@ def main():
     with open(OUTPUT_FILE_PATH, "w") as outfile:
         validations = "\n".join(validator.validate(parser.individuals, parser.families))
 
+        marriage_before_death = Tests.gedcom.Tests.US05.Execute(parser)
+
+        divorce_before_death = Tests.gedcom.Tests.US06.Execute(parser)
+
         individuals_table = printer.print_individuals(parser.individuals.values())
         family_table = printer.print_families(
             parser.families.values(), parser.individuals
@@ -28,6 +32,10 @@ def main():
         print(family_table)
         print("VALIDATIONS")
         print(validations)
+        print("MARRIAGE BEFORE DEATH")
+        print(marriage_before_death)
+        print("DIVORCE BEFORE DEATH")
+        print(divorce_before_death)
 
         outfile.write("INDIVIDUALS\n")
         outfile.write(individuals_table)
@@ -36,14 +44,19 @@ def main():
         outfile.write("\nVALIDATIONS\n")
         outfile.write(validations)
         outfile.write("\n")
+        outfile.write("\nMARRIAGE BEFORE DEATH\n")
+        outfile.write(marriage_before_death)
+        outfile.write("\nDIVORCE BEFORE DEATH\n")
+        outfile.write(divorce_before_death)
 
     Run_Tests(parser)
 
 
 def Run_Tests(hParser):
-    Tests.US03.Execute(hParser)
-    Tests.US04.Execute(hParser)
-
+    Tests.US03.Execute(hParser);
+    Tests.US04.Execute(hParser);
+    Tests.US05.Execute(hParser);
+    Tests.US06.Execute(hParser);
 
 if __name__ == "__main__":
     main()
