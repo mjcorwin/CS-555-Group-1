d07cc8b904bcb21fda8cd344eba299bb9a24df1b
Mark Corwin <mark@carkmorw.in>
2022-10-21 19:41:45 -0400

    feat: Implement US15 with automated tests (#12)
    * feat: Implement US15 with automated tests
    
    * Add back US15
    
    * Update test to be exclusively less than 15

---
+++ gedcom/Tests/US15.py
@@ -0,0 +1,48 @@
+from enum import Enum
+from prettytable import PrettyTable
+
+US15_Problems = []
+
+
+class EUS15_FAILURE(Enum):
+    US15_FAIL_TOO_MANY_SIBLINGS = 0
+
+
+class cUS15_Failure:
+    def __init__(self):
+        self.hFamily = None
+        self.failure_Type = None
+
+    def failed(self):
+        return True if self.failure_Type else False
+
+
+def US15_Test(hParser):
+    US15_Problems.clear()
+
+    print("")
+    print("US15 test failures:")
+
+    for f in hParser.families.values():
+        error = cUS15_Failure()
+
+        if len(f.children_ids) >= 15:
+            error.hFamily = f
+            error.failure_Type = EUS15_FAILURE.US15_FAIL_TOO_MANY_SIBLINGS
+            US15_Problems.append(error)
+
+
+def US15_DisplayResults():
+    pt = PrettyTable()
+    pt.field_names = ["ID", "Number of Siblings", "Data Failure Type"]
+
+    for p in US15_Problems:
+        pt.add_row([p.hFamily.id, len(p.hFamily.children_ids), str(p.failure_Type)])
+
+    print(pt.get_string())
+    return pt.get_string()
+
+
+def Execute(hParser):
+    US15_Test(hParser)
+    return US15_DisplayResults()
---
+++ test/US15_test.py
@@ -0,0 +1,55 @@
+# fewer than 15 siblings in a family
+import random
+
+import pytest
+from gedcom.family import Family
+from gedcom.parser import Parser
+from gedcom.Tests.US15 import *
+
+
+@pytest.fixture
+def family():
+    return Family("F" + str(random.randint(0, 10000)))
+
+
+@pytest.fixture
+def add_siblings_to_family():
+    def add_siblings(family, num_siblings):
+        for _ in range(num_siblings):
+            family.children_ids.append("I" + str(random.randint(0, 10000)))
+
+    return add_siblings
+
+
+class TestUS15:
+    def test_family_with_no_siblings(self, family, add_siblings_to_family):
+        add_siblings_to_family(family, 0)
+        parser = Parser(None)
+        parser.families = {family.id: family}
+
+        US15_Test(parser)
+        assert len(US15_Problems) == 0
+
+    def test_family_with_10_siblings(self, family, add_siblings_to_family):
+        add_siblings_to_family(family, 10)
+        parser = Parser(None)
+        parser.families = {family.id: family}
+
+        US15_Test(parser)
+        assert len(US15_Problems) == 0
+
+    def test_family_with_15_siblings(self, family, add_siblings_to_family):
+        add_siblings_to_family(family, 15)
+        parser = Parser(None)
+        parser.families = {family.id: family}
+
+        US15_Test(parser)
+        assert len(US15_Problems) == 1
+
+    def test_family_with_20_siblings(self, family, add_siblings_to_family):
+        add_siblings_to_family(family, 20)
+        parser = Parser(None)
+        parser.families = {family.id: family}
+
+        US15_Test(parser)
+        assert len(US15_Problems) == 1
